4802ba5d50287ddbee2dbbbb5e1d48c2b5943f89
CATHERINE ABUGAH <Hapsudo>
2025-03-27 10:21:50 +0300

    Practise questions for week 3

---
+++ wk3.sql
@@ -0,0 +1,19 @@
+create database school;
+
+use school;
+
+create table students
+(
+name varchar(100),
+age int,
+gender varchar(10)
+);
+
+
+use school;
+
+insert into students(name,age,gender) values
+("Rupee Don", 15, "Male"),
+("June Dior", 55, "Female"),
+("Elon Musk", 77, "Male")
+
